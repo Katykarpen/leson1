66aa8225bbb33e263a6e32dbfb11c174e936e97f
KarpenEka <fedose-ekaterina@yandex.ru>
2023-06-20 21:17:32 +0300

    Initial commit

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+
+# n = int (input())
+# m = int (input())
+# print(n+m)
+
+# a = int(input())
+# b = int(input())
+# c = a+b
+# print(c)
+
+# a = float(input())
+# b = float(input())
+# c = a+b
+# print(c)
+
+# a = int(input())
+# b = int(input())
+# c = a%b
+# print(c)
+
+name = input('Как твое имя?')
+age =int(input("Cколько тебе лет"))
+
+if age <= 30:
+    primt (f'Привет, {name}!')
+else:
+    print(f'Закрой окно {name}')
+print('зе енд')
